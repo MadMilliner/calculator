ed138ccc31bf26f18e621462f1af3c0b7fa7c060
MadMilliner <jasonapeterson@gmail.com>
2025-06-04 20:23:44 -0700

    Baseline structure in place

---
+++ script.js
@@ -0,0 +1,24 @@
+const add = function(num1, num2) { 
+  return num1 + num2;
+
+};
+
+const subtract = function(num1, num2) {
+	return num1 - num2;
+};
+
+const multiply = function(num1, num2) {
+    return num1 * num2;
+};
+
+const divide = function(num1, num2) {
+    return num1 / num2;
+};
+
+let num1
+let num2
+let operator
+
+const operate = function(num1, num2, operator) {
+
+};
